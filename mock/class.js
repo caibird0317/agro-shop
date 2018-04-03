45966be297758b85c31d78cff2e70dd3302e15f9
caibird0317 <568447838@qq.com>
2018-04-03 17:32:37 +0800

    分类数据调整，新增懒加载，商品选择功能

--- mock/class.js
+++ mock/class.js
@@ -3,49 +3,73 @@ var classMock = {
 			"title": "绿色土豆",
 			"id": "class1",
 			"childlist": [{
-					"url": "xx",
+					"url": "http://wx.qck58.com/productpic/001.jpg",
+					"name": "土豆",
+					"unit": "¥2.50/500g",
+					"less": "0"
+				},
+				{
+					"url": "http://wx.qck58.com/productpic/002.jpg",
 					"name": "土豆",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "http://wx.qck58.com/productpic/003.jpg",
 					"name": "土豆",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "http://wx.qck58.com/productpic/005.jpg",
 					"name": "土豆",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "http://wx.qck58.com/productpic/006.jpg",
 					"name": "土豆",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "http://wx.qck58.com/productpic/008.jpg",
 					"name": "土豆",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "http://wx.qck58.com/productpic/009.jpg",
 					"name": "土豆",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "http://wx.qck58.com/productpic/010.jpg",
 					"name": "土豆",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "http://wx.qck58.com/productpic/011.jpg",
+					"name": "土豆",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "http://wx.qck58.com/productpic/012.jpg",
+					"name": "土豆",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "http://wx.qck58.com/productpic/013.jpg",
+					"name": "土豆",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "http://wx.qck58.com/productpic/014.jpg",
 					"name": "土豆",
 					"unit": "¥2.50/500g",
 					"less": "1000"
@@ -56,43 +80,91 @@ var classMock = {
 			"title": "蔬菜一",
 			"id": "class1",
 			"childlist": [{
-					"url": "xx",
+					"url": "../images/logo.png",
 					"name": "萝卜",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "../images/logo.png",
 					"name": "萝卜",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "../images/logo.png",
 					"name": "萝卜",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "../images/logo.png",
 					"name": "萝卜",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "../images/logo.png",
 					"name": "萝卜",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "../images/logo.png",
 					"name": "萝卜",
 					"unit": "¥2.50/500g",
 					"less": "1000"
 				},
 				{
-					"url": "xx",
+					"url": "../images/logo.png",
+					"name": "萝卜",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "../images/logo.png",
+					"name": "萝卜",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "../images/logo.png",
+					"name": "萝卜",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "../images/logo.png",
+					"name": "萝卜",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "../images/logo.png",
+					"name": "萝卜",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "../images/logo.png",
+					"name": "萝卜",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "../images/logo.png",
+					"name": "萝卜",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "../images/logo.png",
+					"name": "萝卜",
+					"unit": "¥2.50/500g",
+					"less": "1000"
+				},
+				{
+					"url": "../images/logo.png",
 					"name": "萝卜",
 					"unit": "¥2.50/500g",
 					"less": "1000"
@@ -103,7 +175,7 @@ var classMock = {
 			"title": "蔬菜一",
 			"id": "class1",
 			"childlist": [{
-				"url": "xx",
+				"url": "../images/free.png",
 				"name": "土豆",
 				"unit": "¥2.50/500g",
 				"less": "1000"
